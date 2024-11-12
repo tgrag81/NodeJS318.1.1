ec7c43273513c63aab888d16ffd848d0dae46569
Terence A Gragston <terence.gragston@gmail.com>
2024-11-12 07:33:26 -0500

    Add files via upload

--- node.js
+++ node.js
@@ -1,9 +1,5 @@
-const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res)) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World!\n');
-});
+function helloWorld() {
+    console.log("Hello, World!");
+  }
+  
+  helloWorld(); // Call the function to print "Hello, World!" to the console
